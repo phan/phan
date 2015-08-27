04287fd63c86e6fda701bf54975ad0e6fec2cab9
Rasmus Lerdorf <rasmus@php.net>
2015-08-27 15:50:37 -0400

    Walk all the elements of a list() inside a foreach

--- includes/pass2.php
+++ includes/pass2.php
@@ -154,8 +154,9 @@ function pass2($file, $namespace, $ast, $current_scope, $parent_node=null, $curr
 					Log::err(Log::EFATAL, "Can't use list() as a key element - aborting", $file, $ast->lineno);
 				}
 				if($ast->children[1]->kind == \ast\AST_LIST) {
-					add_var_scope($current_scope, var_name($ast->children[1]->children[0]), '', true);
-					add_var_scope($current_scope, var_name($ast->children[1]->children[1]), '', true);
+					foreach($ast->children[1]->children as $node) {
+						add_var_scope($current_scope, var_name($node), '', true);
+					}
 					if(!empty($ast->children[2])) {
 						add_var_scope($current_scope, var_name($ast->children[2]), '', true);
 					}
