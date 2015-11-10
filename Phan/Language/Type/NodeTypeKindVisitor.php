45a71f274cc98950fa58a12938ba13077e7389d6
Andrew Morrison <amorrison@etsy.com>
2015-11-10 22:34:48 +0000

    Giving up on getting types for shit like $foo->$name()

--- Phan/Language/Type/NodeTypeKindVisitor.php
+++ Phan/Language/Type/NodeTypeKindVisitor.php
@@ -576,6 +576,18 @@ class NodeTypeKindVisitor extends KindVisitorImplementation {
 
         $method_name = $node->children['method'];
 
+        // Give up on any complicated nonsense where the
+        // method name is a variable such as in
+        // `$variable->$function_name()`.
+        if ($method_name instanceof Node) {
+            return new UnionType();
+        }
+
+        // Method names can some times turn up being
+        // other method calls.
+        assert(is_string($method_name),
+            "Method name must be a string. Something else given.");
+
         if (!$clazz->hasMethodWithName($method_name)) {
             Log::err(
                 Log::EUNDEF,
