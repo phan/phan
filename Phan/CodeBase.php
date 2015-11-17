25951191df5624a758cfa71f1ffbb8c5361f72ab
Andrew Morrison <amorrison@etsy.com>
2015-11-17 20:30:23 +0000

    Adding versioning to the serialized code base

--- Phan/CodeBase.php
+++ Phan/CodeBase.php
@@ -39,6 +39,14 @@ use \Phan\Language\FQSEN;
 class CodeBase {
 
     /**
+     * Set a version on this class so that we can
+     * error out when reading old versions of serialized
+     * files
+     */
+    const CODE_BASE_VERSION = 2;
+    private $code_base_version;
+
+    /**
      * @var File[]
      * A map from file name to info such as its last
      * modification date used to determine if a file
@@ -78,6 +86,12 @@ class CodeBase {
         $this->addClassesByNames($internal_trait_name_list);
         $this->addFunctionsByNames($internal_function_name_list);
         $this->resetSummary();
+
+        // Set a version on this class so that we can
+        // error out when reading old versions of serialized
+        // files
+        $this->code_base_version =
+            CodeBase::CODE_BASE_VERSION;
     }
 
     /**
@@ -261,29 +275,6 @@ class CodeBase {
     }
 
     /**
-     * Remove any objects we have associated with the
-     * given file so that we can re-read it
-     *
-     * @return null
-     */
-    public function flushDependenciesForFile(string $file_path) {
-        if (empty($this->file_map[$file_path])) {
-            $this->file_map[$file_path] = new File($file_path);
-            return;
-        }
-
-        $code_file = $this->file_map[$file_path];
-
-        foreach ($code_file->getClassFQSENList() as $fqsen) {
-            unset($this->class_map[(string)$fqsen]);
-        }
-
-        foreach ($code_file->getMethodFQSENList() as $fqsen) {
-            unset($this->method_map[(string)$fqsen]);
-        }
-    }
-
-    /**
      * @param string $file_path
      * A path to a file name
      *
@@ -296,6 +287,26 @@ class CodeBase {
         }
 
         return $this->file_map[$file_path];
+    }
+
+    /**
+     * Remove any objects we have associated with the
+     * given file so that we can re-read it
+     *
+     * @return null
+     */
+    public function flushDependenciesForFile(string $file_path) {
+        $code_file = $this->getFileForFile($file_path);
+
+        // Flush all classes from the file
+        foreach ($code_file->getClassFQSENList() as $fqsen) {
+            unset($this->class_map[(string)$fqsen]);
+        }
+
+        // Flush all methods from the file
+        foreach ($code_file->getMethodFQSENList() as $fqsen) {
+            unset($this->method_map[(string)$fqsen]);
+        }
     }
 
     /**
@@ -376,6 +387,14 @@ class CodeBase {
     }
 
     /**
+     * @return int
+     * The version number of this code base
+     */
+    public function getVersion() : int {
+        return $this->code_base_version ?? -1;
+    }
+
+    /**
      * @return CodeBase|bool
      * A stored code base if its successful or false if
      * unserialize fucks up
@@ -385,10 +404,18 @@ class CodeBase {
             throw new \Exception("No serialized_code_base_file defined");
         }
 
-        return unserialize(
+        $code_base = unserialize(
             file_get_contents(
                 Config::get()->serialized_code_base_file
             )
         );
+
+        if ($code_base->getVersion() !== self::CODE_BASE_VERSION) {
+            throw new \Exception(
+                "Stored code base has bad version number {$code_base->getVersion()}"
+            );
+        }
+
+        return $code_base;
     }
 }
