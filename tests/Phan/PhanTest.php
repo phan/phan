6c1b0bffcd3f610b890f345a7f88336219c651bb
Rasmus Lerdorf <rasmus@php.net>
2015-12-01 11:18:10 -0800

    Proper regex support in tests

--- tests/Phan/PhanTest.php
+++ tests/Phan/PhanTest.php
@@ -83,16 +83,63 @@ class PhanTest extends \PHPUnit_Framework_TestCase {
                 // print "\n" . $exception->getMessage() . "\n";
             }
 
-            $output = str_replace(
-                $test_file_path, '%s',
-                trim(ob_get_clean())
-            );
+            $output = trim(ob_get_clean());
 
+            $wanted_re = preg_replace('/\r\n/', "\n", $expected_output);
+			// do preg_quote, but miss out any %r delimited sections
+            $temp = "";
+            $r = "%r";
+            $startOffset = 0;
+            $length = strlen($wanted_re);
+            while($startOffset < $length) {
+                $start = strpos($wanted_re, $r, $startOffset);
+                if ($start !== false) {
+                    // we have found a start tag
+                    $end = strpos($wanted_re, $r, $start+2);
+                    if ($end === false) {
+                        // unbalanced tag, ignore it.
+                        $end = $start = $length;
+                    }
+                } else {
+                    // no more %r sections
+                    $start = $end = $length;
+                }
+                // quote a non re portion of the string
+                $temp = $temp . preg_quote(substr($wanted_re, $startOffset, ($start - $startOffset)),  '/');
+                // add the re unquoted.
+                if ($end > $start) {
+                    $temp = $temp . '(' . substr($wanted_re, $start+2, ($end - $start-2)). ')';
+                }
+                $startOffset = $end + 2;
+            }
+            $wanted_re = $temp;
+            $wanted_re = str_replace(['%binary_string_optional%'], 'string', $wanted_re);
+            $wanted_re = str_replace(['%unicode_string_optional%'], 'string', $wanted_re);
+            $wanted_re = str_replace(['%unicode\|string%', '%string\|unicode%'], 'string', $wanted_re);
+            $wanted_re = str_replace(['%u\|b%', '%b\|u%'], '', $wanted_re);
+            // Stick to basics
+            $wanted_re = str_replace('%e', '\\' . DIRECTORY_SEPARATOR, $wanted_re);
+            $wanted_re = str_replace('%s', '[^\r\n]+', $wanted_re);
+            $wanted_re = str_replace('%S', '[^\r\n]*', $wanted_re);
+            $wanted_re = str_replace('%a', '.+', $wanted_re);
+            $wanted_re = str_replace('%A', '.*', $wanted_re);
+            $wanted_re = str_replace('%w', '\s*', $wanted_re);
+            $wanted_re = str_replace('%i', '[+-]?\d+', $wanted_re);
+            $wanted_re = str_replace('%d', '\d+', $wanted_re);
+            $wanted_re = str_replace('%x', '[0-9a-fA-F]+', $wanted_re);
+            $wanted_re = str_replace('%f', '[+-]?\.?\d+\.?\d*(?:[Ee][+-]?\d+)?', $wanted_re);
+            $wanted_re = str_replace('%c', '.', $wanted_re);
+            // %f allows two points "-.0.0" but that is the best *simple* expression
+
+            $this->assertRegExp("/^$wanted_re\$/", $output, "Unexpected output in $test_file_path");
+
+/*
             $this->assertEquals(
                 $expected_output,
                 $output,
                 "Unexpected output in $test_file_path"
             );
+*/
         }
     }
 }
