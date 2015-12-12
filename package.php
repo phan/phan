426eef4c6fd750412fb07028abd279034da87f17
Pavel Batanov <pavel@batanov.me>
2015-12-12 13:25:41 +0300

    Phar packaging
    * Script to package all php files to phar
    * Travis configuration update to push phar to releases

---
+++ package.php
@@ -0,0 +1,10 @@
+<?php
+
+// add all files in the project
+$dir = dirname(__FILE__);
+
+// create with alias "project.phar"
+$phar = new Phar('build/phan.phar', 0, 'phan.phar');
+
+$phar->buildFromDirectory($dir . '/', '/\.php$/');
+$phar->setStub($phar->createDefaultStub('phan.php'));
