91aa91252f1fdf6ff2546c5ac5f55b03c8d1f07b
Jordi Boggiano <j.boggiano@seld.be>
2015-12-14 10:23:42 +0000

    Add test that should pass without warning

--- tests/files/src/0047_backwards_compatibility.php
+++ tests/files/src/0047_backwards_compatibility.php
@@ -3,3 +3,11 @@ echo $foo->$bar['baz'];
 Foo::$bar['baz']();
 $foo->$bar['baz']();
 strlen($foo->$bar['baz']);
+
+// tests that should pass without warning below
+class Test {
+    public static $vals = array('a' => 'A', 'b' => 'B');
+    public static function get($letter) {
+        return self::$vals[$letter];
+    }
+}
