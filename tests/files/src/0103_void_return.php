80b939541ced0ae845571d5248decca8a7dba4ff
Andrew Morrison <amorrison@etsy.com>
2016-01-11 17:58:54 +0000

    Closes #44 - Adding a test for distinguishing null and void return types.

---
+++ tests/files/src/0103_void_return.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @return null */
+function f0() { return null; }
+
+/** @return null */
+function f1() { return 42; }
+
+/** @return null */
+function f2() {}
+
+/** @return void */
+function g0() { return null; }
+
+/** @return void */
+function g1() { return 42; }
+
+/** @return void */
+function g2() {}
+
