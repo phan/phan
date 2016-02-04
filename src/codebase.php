92040946a18fcb3d11a2245a497acee2ddef7c6d
Cameron Eagans <me@cweagans.net>
2016-02-04 12:26:45 -0700

    Use the global composer autoloader if phan is installed globally.

--- src/codebase.php
+++ src/codebase.php
@@ -6,7 +6,15 @@ $internal_interface_name_list = get_declared_interfaces();
 $internal_trait_name_list = get_declared_traits();
 $internal_function_name_list = get_defined_functions()['internal'];
 
-require_once __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+  // This is the normal path when Phan is installed only in the scope of a project.
+  require_once __DIR__ . '/../vendor/autoload.php';
+}
+else {
+  // This is the path to autoload.php when Phan is installed globally.
+  require_once __DIR__ . '/../../../autoload.php';
+}
 
 use Phan\CodeBase;
 
