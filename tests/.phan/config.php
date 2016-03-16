d83f71f66489891437d66f61c518b5ab3b49460a
Andrew Morrison <amorrison@etsy.com>
2016-03-16 17:18:27 +0000

    Adding .phan/config.php to tests directory for making one-off tests easier

---
+++ tests/.phan/config.php
@@ -0,0 +1,100 @@
+<?php
+
+use \Phan\Config;
+use \Phan\Issue;
+
+/**
+ * This configuration will be read and overlayed on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ *
+ * @see src/Phan/Config.php
+ * See Config for all configurable options.
+ *
+ * A Note About Paths
+ * ==================
+ *
+ * Files referenced from this file should be defined as
+ *
+ * ```
+ *   Config::projectPath('relative_path/to/file')
+ * ```
+ *
+ * where the relative path is relative to the root of the
+ * project which is defined as either the working directory
+ * of the phan executable or a path passed in via the CLI
+ * '-d' flag.
+ */
+return [
+
+    // If true, missing properties will be created when
+    // they are first seen. If false, we'll report an
+    // error message.
+    "allow_missing_properties" => false,
+
+    // Allow null to be cast as any type and for any
+    // type to be cast to null.
+    "null_casts_as_any_type" => false,
+
+    // Backwards Compatibility Checking
+    'backward_compatibility_checks' => false,
+
+    // If enabled, check all methods that override a
+    // parent method to make sure its signature is
+    // compatible with the parent's. This check
+    // can add quite a bit of time to the analysis.
+    'analyze_signature_compatibility' => true,
+
+    // Set to true in order to attempt to detect dead
+    // (unreferenced) code. Keep in mind that the
+    // results will only be a guess given that classes,
+    // properties, constants and methods can be referenced
+    // as variables (like `$class->$property` or
+    // `$class->$method()`) in ways that we're unable
+    // to make sense of.
+    'dead_code_detection' => false,
+
+    // Run a quick version of checks that takes less
+    // time
+    "quick_mode" => false,
+
+    // The minimum severity level to report on. This can be
+    // set to Issue::SEVERITY_LOW, Issue::SEVERITY_NORMAL or
+    // Issue::SEVERITY_CRITICAL.
+    'minimum_severity' => Issue::SEVERITY_LOW,
+
+    // A list of files to include in analysis
+    'file_list' => [
+        // 'vendor/phpunit/phpunit/src/Framework/TestCase.php',
+    ],
+
+    // The number of processes to fork off during the analysis
+    // phase.
+    'processes' => 1,
+
+    // A list of directories that should be parsed for class and
+    // method information. After excluding the directories
+    // defined in exclude_analysis_directory_list, the remaining
+    // files will be statically analyzed for errors.
+    //
+    // Thus, both first-party and third-party code being used by
+    // your application should be included in this list.
+    'directory_list' => [
+    ],
+
+    // A directory list that defines files that will be excluded
+    // from static analysis, but whose class and method
+    // information should be included.
+    //
+    // Generally, you'll want to include the directories for
+    // third-party code (such as "vendor/") in this list.
+    //
+    // n.b.: If you'd like to parse but not analyze 3rd
+    //       party code, directories containing that code
+    //       should be added to the `directory_list` as
+    //       to `excluce_analysis_directory_list`.
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+
+];
