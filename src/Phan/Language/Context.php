70f1b739fc6dbdd6115809e6393a92a7d9dedbca
Andrew Morrison <amorrison@etsy.com>
2016-04-26 21:45:25 +0000

    Possibly helps Issue #240

--- src/Phan/Language/Context.php
+++ src/Phan/Language/Context.php
@@ -274,6 +274,10 @@ class Context extends FileRef implements \Serializable
      */
     public function getClassFQSEN() : FullyQualifiedClassName
     {
+
+        assert($this->getScope()->hasParentScope(),
+            "No parent scope from $this");
+
         return $this->getScope()->getClassFQSEN();
     }
 
