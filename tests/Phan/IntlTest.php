111ecd63f330aee69970bb4357d26159902db64b
Andrew Morrison <amorrison@etsy.com>
2016-06-17 20:53:48 +0000

    Making ResourceBundle tests optional

---
+++ tests/Phan/IntlTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+namespace Phan\Tests;
+
+use Phan\CodeBase;
+use Phan\Language\Type;
+
+/**
+ * @requires extension intl
+ */
+class IntlTest extends AbstractPhanFileTest {
+
+    /**
+     * This reads all files in `tests/files/src`, runs
+     * the analyzer on each and compares the output
+     * to the files's counterpart in
+     * `tests/files/expected`
+     *
+     * @param string[] $test_file_list
+     * @param string $expected_file_path
+     *
+     * @dataProvider getTestFiles
+     */
+    public function testFiles($test_file_list, $expected_file_path) {
+        parent::testFiles($test_file_list, $expected_file_path);
+    }
+
+    public function getTestFiles() {
+        return $this->scanSourceFilesDir(INTL_TEST_FILE_DIR, INTL_EXPECTED_DIR);
+    }
+}
