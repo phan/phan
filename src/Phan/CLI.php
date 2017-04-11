97e816aa9f4bf136a6ff5d5ae3c274e60fad6b22
Adam Baratz <adam.baratz@gmail.com>
2017-04-11 09:55:32 -0400

    Change -h option so it doesn't require value

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -61,7 +61,7 @@ class CLI
         // Parse command line args
         // still available: g,n,t,u,w
         $opts = getopt(
-            "f:m:o:c:k:aeqbr:pid:3:y:l:xj:zh:v",
+            "f:m:o:c:k:aeqbr:pid:3:y:l:xj:zhv",
             [
                 'backward-compatibility-checks',
                 'color',
