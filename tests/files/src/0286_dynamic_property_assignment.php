7eeaf5322d5bc0bb86db31aa904a4facba9a0cd2
Andrew S. Morrison <amorrison@etsy.com>
2017-04-21 17:19:21 +0000

    Tweaks from review

--- tests/files/src/0286_dynamic_property_assignment.php
+++ tests/files/src/0286_dynamic_property_assignment.php
@@ -3,9 +3,9 @@ class C286 {
     function f() {}
 }
 
-$v0 = new \stdClass;
-$v0->p = 42;
+$v286a = new \stdClass;
+$v286a->p286 = 42;
 
-$v1 = new \stdClass;
-$v1->p = new C286();
-$v1->p->f();
+$v286b= new \stdClass;
+$v286b->p286 = new C286();
+$v286b->p286->f();
