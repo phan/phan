8a294ac18e699556e273ae6a4b2afa88822c5a24
Tyson Andre <tysonandre775@hotmail.com>
2017-06-03 18:27:00 -0700

    Improve code coverage of comments, fix test expectation format

---
+++ tests/files/src/0300_misc_types.php
@@ -0,0 +1,31 @@
+<?php
+
+function expect_string_300(string $x) { }
+function expect_int_300(int $x) { }
+
+function test300() {
+    $intVar = 42;
+    $strVar = 'x';
+    // Every one of these statements should warn about incorrect types, unless commented otherwise
+    expect_string_300(++$intVar);
+    expect_string_300($intVar++);
+    expect_string_300(--$intVar);
+    expect_string_300($intVar--);
+    expect_int_300($strVar++);
+    expect_int_300(++$strVar);
+    expect_int_300($strVar--);
+    expect_int_300(--$strVar);
+
+    clone(42);  // TODO: Warn
+    expect_string_300(clone(new stdClass()));
+    expect_string_300($refIntVar &= $intVar);
+    if (false) {
+        expect_int_300(`ls`);
+    }
+    expect_string_300((object)['key' => 'val']);
+    expect_string_300(null ? 'string' : 3);  // null is always falsey, so Phan infers the type as `int`
+    expect_string_300(null ?: 3);  // null is always falsey, so Phan infers the type as `int`
+    expect_default_int_300('x');
+
+}
+self::missingAndNotInClassScope();
