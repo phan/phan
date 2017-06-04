d4cf6d76abc03eef7b2d57eef9d516306367e503
Tyson Andre <tysonandre775@hotmail.com>
2017-06-03 18:40:08 -0700

    Update phpunit (and other deps) from 5.7 to latest stable 6.2
    PHPUnit supports 6.2, and is more likely to have up to date
    documentation, bug fixes, code coverage generation, etc.

--- tests/Phan/Library/TupleTest.php
+++ tests/Phan/Library/TupleTest.php
@@ -1,12 +1,28 @@
 <?php declare(strict_types = 1);
-namespace Phan\Tests;
+namespace Phan\Tests\Library;
 
+use Phan\Tests\BaseTest;
+use Phan\Library\Tuple1;
+use Phan\Library\Tuple2;
 
 /**
  */
 class TupleTest extends BaseTest
 {
-    public function testSimple()
+    public function testSimple1()
     {
+        $x = new Tuple1('value');
+        $this->assertSame('value', $x->_0);
+        $this->assertSame(['value'], $x->toArray());
+        $this->assertSame(1, $x->arity());
+    }
+
+    public function testSimple2()
+    {
+        $x = new Tuple2('value', 42);
+        $this->assertSame('value', $x->_0);
+        $this->assertSame(42, $x->_1);
+        $this->assertSame(['value', 42], $x->toArray());
+        $this->assertSame(2, $x->arity());
     }
 }
