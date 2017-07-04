53a17f333970227073fe8f0d8a67fb401b5e2330
Tyson Andre <tysonandre775@hotmail.com>
2017-07-03 17:51:32 -0700

    Fixes #975 - There are no php4 constructors for classes in namespaces.
    If a class is not in the global namespaces,
    the PHP interpreter does not support PHP4 constructors.
    Make Phan's analysis reflect that.

--- tests/files/src/0325_php4_construct.php
+++ tests/files/src/0325_php4_construct.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace {
 class Bar325 {
     /** @return int */
     public function Bar325(array $arg) {
@@ -17,3 +18,24 @@ class Foo325 extends Bar325 {
 // error happens with/without the below lines.
 $f325 = new Bar325(11);
 $f325 = new Foo325(11);
+}  // end of global namespace
+
+namespace NS325 {
+class X325 {
+    /** @return int (should not warn about being static) */
+    public static function X325(array $arg) {
+        return 42;
+    }
+}
+
+class Y325 extends X325 {
+    public $arg;
+    public function __construct(int $arg) {
+        parent::__construct($arg);  // should warn that it doesn't exist
+        parent::X325($arg);  // should warn that it is incompatible
+    }
+}
+// error happens with/without the below lines.
+$f325 = new X325(11);  // PhanParamTooMany
+$f325 = new Y325(11);
+}  // end of NS325
