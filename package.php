594b088bdf036f317cc006d7afe880ecf4c5f789
Tyson Andre <tysonandre775@hotmail.com>
2018-01-20 13:35:25 -0800

    Automate the script to build a phar file

--- package.php
+++ package.php
@@ -3,8 +3,13 @@
 // add all files in the project
 $dir = dirname(__FILE__);
 
+if (!file_exists('build')) {
+    echo "Creating build/\n";
+    mkdir('build') || die('unable to ensure build/ directory exists');
+}
 // create with alias "project.phar"
 $phar = new Phar('build/phan.phar', 0, 'phan.phar');
 
 $phar->buildFromDirectory($dir . '/', '/\.php$/');
 $phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub('src/phan.php'));
+echo "Created phar in build/phan.phar\n";
