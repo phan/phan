c634db77864d943c0edd0a213d00b63f0865a8e4
Tyson Andre <tysonandre775@hotmail.com>
2018-01-30 08:55:07 -0800

    Fixes #1410: NonBoolBranchPlugin doesn't account for true/false
    (Those union types were added to the type system, but
    NonBoolBranchPlugin was not updated and still checked for exactly 'bool'.

---
+++ tests/plugin_test/src/020_bool.php
@@ -0,0 +1,24 @@
+<?php
+
+function maybe_bool() : ?bool {
+    switch (rand() % 3) {
+    case 0: return true;
+    case 1: return false;
+    default: return null;
+    }
+}
+/** @return array|bool */
+function maybe_array() {
+    switch (rand() % 3) {
+    case 0: return true;
+    case 1: return false;
+    default: return [33];
+    }
+}
+if (true) {}
+if (FAlse) {}
+if (PHP_VERSION_ID) {}
+if (null) {
+} else if ($x = maybe_bool()) {
+} else if ($x = maybe_array()) {
+}
