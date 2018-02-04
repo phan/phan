ddd0ddcc48b18bdc2d8f69829915a443737da5b8
Tyson Andre <tysonandre775@hotmail.com>
2018-02-04 15:19:51 -0800

    Fixes #1325 : Create full TextDocumentSyncOptions
    The save notification wasn't getting sent with TextDocumentSyncKind::NONE.
    
    (TextDocumentSyncOptions might not be supported in older language server
    protocol clients. But vscode-php-phan is bundled with the latest client
    library, so it should work)

--- src/Phan/LanguageServer/LanguageServer.php
+++ src/Phan/LanguageServer/LanguageServer.php
@@ -15,9 +15,11 @@ use Phan\LanguageServer\Protocol\InitializeResult;
 use Phan\LanguageServer\Protocol\Message;
 use Phan\LanguageServer\Protocol\Position;
 use Phan\LanguageServer\Protocol\Range;
+use Phan\LanguageServer\Protocol\SaveOptions;
 use Phan\LanguageServer\Protocol\ServerCapabilities;
 use Phan\LanguageServer\Protocol\TextDocumentIdentifier;
 use Phan\LanguageServer\Protocol\TextDocumentSyncKind;
+use Phan\LanguageServer\Protocol\TextDocumentSyncOptions;
 use Phan\LanguageServer\Server\TextDocument;
 use Phan\LanguageServer\ProtocolReader;
 use Phan\LanguageServer\ProtocolWriter;
@@ -515,10 +517,13 @@ class LanguageServer extends AdvancedJsonRpc\Dispatcher
                 );
             }
 
+
             $serverCapabilities = new ServerCapabilities();
+
             // FULL: Ask the client to return always return full documents (because we need to rebuild the AST from scratch)
             // NONE: Don't sync until the user explitly saves a document.
-            $serverCapabilities->textDocumentSync = Config::getValue('language_server_analyze_only_on_save') ? TextDocumentSyncKind::NONE : TextDocumentSyncKind::FULL;
+            $serverCapabilities->textDocumentSync = $this->makeTextDocumentSyncOptions();
+
             // TODO: Support "Find all symbols"?
             //$serverCapabilities->documentSymbolProvider = true;
             // TODO: Support "Find all symbols in workspace"?
@@ -544,6 +549,17 @@ class LanguageServer extends AdvancedJsonRpc\Dispatcher
 
             return new InitializeResult($serverCapabilities);
         });
+    }
+
+    private function makeTextDocumentSyncOptions() : TextDocumentSyncOptions {
+        $textDocumentSyncOptions = new TextDocumentSyncOptions();
+        $textDocumentSyncOptions->openClose = true;
+        $textDocumentSyncOptions->change = Config::getValue('language_server_analyze_only_on_save') ? TextDocumentSyncKind::NONE : TextDocumentSyncKind::FULL;
+
+        $saveOptions = new SaveOptions();
+        $saveOptions->includeText = true;
+        $textDocumentSyncOptions->save = $saveOptions;
+        return $textDocumentSyncOptions;
     }
 
     public function initialized()
