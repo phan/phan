2526d9166393e7c427dfa1ee985460eb1da63db4
Tyson Andre <tysonandre775@hotmail.com>
2018-05-26 17:43:05 -0700

    Add a test of unused variable definitions within loops

--- tests/plugin_test/src/033_loop_usage.php
+++ tests/plugin_test/src/033_loop_usage.php
@@ -10,4 +10,12 @@ function example_loop() {
         $b = $a;
     }
 }
+function example_loop2() {
+    foreach ([2,3] as $x) {
+        $a = $x;
+        $a = 2 + $x;
+        echo $a;
+    }
+}
 example_loop();
+example_loop2();
