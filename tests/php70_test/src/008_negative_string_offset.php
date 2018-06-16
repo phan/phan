7b4555d49810bfbf267c4cd039281c7f9465b512
Oliver Maksimović <maksimovic@outlook.com>
2018-06-16 20:24:58 +0200

    work in progress - the test will fail
    also, expected values aren't real (yet)

--- tests/php70_test/src/008_negative_string_offset.php
+++ tests/php70_test/src/008_negative_string_offset.php
@@ -1,26 +1,58 @@
 <?php
 
-class class_008 {
+/** @var string $str */
+$str = "string";
+$char = $str{-1};
+
+class class_008_parent {
+
+    /** @var string  */
+    private $parent_string = "string";
+
+    public function getParentString() : string
+    {
+        return $this->parent_string;
+    }
+
+}
+
+class class_008 extends class_008_parent {
+
+    /** @var string */
+    public $string;
+
+    /** @var string */
+    public static $static_string;
+
+    /** @var string */
+    const STR = "classconst";
+
+    public function __construct()
+    {
+        $this->string = "string";
+        self::$static_string = "string";
+    }
 
     public function testStrCurlyBrackets(string $param1, string $param2) : string
     {
-        $temp = $param1{-2};
-        return $temp . $param2{"-3"};
+        $temp = $param1{-1};
+        return $temp . $param2{"-2"};
     }
 
     /**
-     * @param string $param
      * @return bool
      */
-    public function testStrSquareBrackets(string $param) : bool
+    public function testStrSquareBrackets() : bool
     {
-        $one = $param["-1"];
-        return $one || in_array($param[-4], [1, 2, 3]);
+        $var = self::STR["-3"];
+        $var .= self::$static_string[-4];
+        $var .= parent::getParentString()[-5];
+        return $var && in_array($this->string[-5], [1, 2, 3]);
     }
 
-    public function testArray(array $array)
+    public function testArray(array $arr)
     {
-        return $array[-1];
+        return $arr[-1];
     }
 
 }
@@ -28,5 +60,5 @@ class class_008 {
 $c = new class_008();
 
 $c->testStrCurlyBrackets("param1", "param2");
-$c->testStrSquareBrackets("param");
+$c->testStrSquareBrackets();
 $c->testArray([-1 => 1]);
