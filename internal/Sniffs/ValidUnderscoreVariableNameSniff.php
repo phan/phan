14f9d287c56feca5dd6135923953d92c9be6c717
Tyson Andre <tysonandre775@hotmail.com>
2018-07-08 19:35:15 -0700

    Make the sniffer use the same style as Phan

--- internal/Sniffs/ValidUnderscoreVariableNameSniff.php
+++ internal/Sniffs/ValidUnderscoreVariableNameSniff.php
@@ -12,7 +12,6 @@
 
 namespace PHP_CodeSniffer\Standards\Phan\Sniffs\NamingConventions;
 
-// use PHP_CodeSniffer\Standards\Zend\Sniffs\NamingConventions;
 use PHP_CodeSniffer\Sniffs\AbstractVariableSniff;
 use PHP_CodeSniffer\Files\File;
 
@@ -23,30 +22,30 @@ class ValidUnderscoreVariableNameSniff extends AbstractVariableSniff
     /**
      * Processes this test, when one of its tokens is encountered.
      *
-     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-     * @param int                         $stackPtr  The position of the current token in the
-     *                                               stack passed in $tokens.
+     * @param \PHP_CodeSniffer\Files\File $phpcs_file The file being scanned.
+     * @param int                         $stack_ptr  The position of the current token in the
+     *                                                stack passed in $tokens.
      *
      * @return void
      */
-    protected function processVariable(File $phpcsFile, $stackPtr)
+    protected function processVariable(File $phpcs_file, $stack_ptr)
     {
-        if ($this->isExcluded($phpcsFile)) {
+        if ($this->isExcluded($phpcs_file)) {
             return;
         }
-        $tokens  = $phpcsFile->getTokens();
-        $varName = ltrim($tokens[$stackPtr]['content'], '$');
+        $tokens  = $phpcs_file->getTokens();
+        $var_name = ltrim($tokens[$stack_ptr]['content'], '$');
 
         // If it's a php reserved var, then its ok.
-        if (isset($this->phpReservedVars[$varName]) === true) {
+        if (isset($this->phpReservedVars[$var_name]) === true) {
             return;
         }
 
-        $objOperator = $phpcsFile->findNext([T_WHITESPACE], ($stackPtr + 1), null, true);
-        if ($tokens[$objOperator]['code'] === T_OBJECT_OPERATOR) {
+        $obj_operator = $phpcs_file->findNext([T_WHITESPACE], ($stack_ptr + 1), null, true);
+        if ($tokens[$obj_operator]['code'] === T_OBJECT_OPERATOR) {
             // Check to see if we are using a variable from an object.
             // TODO: This workaround doesn't check variables that are the expression of a member property access expression
-            $var = $phpcsFile->findNext([T_WHITESPACE], ($objOperator + 1), null, true);
+            $var = $phpcs_file->findNext([T_WHITESPACE], ($obj_operator + 1), null, true);
             if ($tokens[$var]['code'] === T_STRING) {
                 return;
             }//end if
@@ -55,126 +54,125 @@ class ValidUnderscoreVariableNameSniff extends AbstractVariableSniff
         // There is no way for us to know if the var is public or private,
         // so we have to ignore a leading underscore if there is one and just
         // check the main part of the variable name.
-        $originalVarName = $varName;
-        if (substr($varName, 0, 1) === '_') {
+        $original_var_name = $var_name;
+        if (substr($var_name, 0, 1) === '_') {
             // Let PSR-12 checks deal with this
             return;
         }
 
-        if (self::isUnderscoreVariableName($originalVarName) === false) {
+        if (self::isUnderscoreVariableName($original_var_name) === false) {
             $error = 'Variable "%s" is not in valid underscore name format';
-            $data  = [$originalVarName];
-            $phpcsFile->addError($error, $stackPtr, 'NotUnderscore', $data);
-        } else if (preg_match('|\d|', $varName) === 1) {
+            $data  = [$original_var_name];
+            $phpcs_file->addError($error, $stack_ptr, 'NotUnderscore', $data);
+        } elseif (preg_match('|\d|', $var_name) === 1) {
             $warning = 'Variable "%s" contains numbers but this is discouraged';
-            $data    = [$originalVarName];
-            $phpcsFile->addWarning($warning, $stackPtr, 'ContainsNumbers', $data);
+            $data    = [$original_var_name];
+            $phpcs_file->addWarning($warning, $stack_ptr, 'ContainsNumbers', $data);
         }
-
     }//end processVariable()
 
 
     /**
      * Processes class member variables.
      *
-     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-     * @param int                         $stackPtr  The position of the current token in the
+     * @param \PHP_CodeSniffer\Files\File $phpcs_file The file being scanned.
+     * @param int                         $stack_ptr  The position of the current token in the
      *                                               stack passed in $tokens.
      *
      * @return void
      */
-    protected function processMemberVar(File $phpcsFile, $stackPtr)
+    protected function processMemberVar(File $phpcs_file, $stack_ptr)
     {
-        if ($this->isExcluded($phpcsFile)) {
+        if ($this->isExcluded($phpcs_file)) {
             return;
         }
-        $tokens      = $phpcsFile->getTokens();
-        $varName     = ltrim($tokens[$stackPtr]['content'], '$');
-        $memberProps = $phpcsFile->getMemberProperties($stackPtr);
-        if (empty($memberProps) === true) {
+        $tokens      = $phpcs_file->getTokens();
+        $var_name     = ltrim($tokens[$stack_ptr]['content'], '$');
+        $member_props = $phpcs_file->getMemberProperties($stack_ptr);
+        if (empty($member_props) === true) {
             // Exception encountered.
             return;
         }
 
-        $public = ($memberProps['scope'] === 'public');
+        $public = ($member_props['scope'] === 'public');
 
-        if (substr($varName, 0, 1) === '_') {
+        if (substr($var_name, 0, 1) === '_') {
             // Phan's coding style uses PSR-12.
             // PSR-12 already checks for this, so skip these
             return;
         }
 
         // Remove a potential underscore prefix for testing CamelCaps.
-        if (self::isUnderscoreVariableName($varName) === false) {
+        if (self::isUnderscoreVariableName($var_name) === false) {
             $error = 'Member variable "%s" is not in valid underscore format';
-            $data  = [$varName];
-            $phpcsFile->addError($error, $stackPtr, 'MemberVarNotUnderscore', $data);
-        } else if (preg_match('|\d|', $varName) === 1) {
+            $data  = [$var_name];
+            $phpcs_file->addError($error, $stack_ptr, 'MemberVarNotUnderscore', $data);
+        } elseif (preg_match('|\d|', $var_name) === 1) {
             $warning = 'Member variable "%s" contains numbers but this is discouraged';
-            $data    = [$varName];
-            $phpcsFile->addWarning($warning, $stackPtr, 'MemberVarContainsNumbers', $data);
+            $data    = [$var_name];
+            $phpcs_file->addWarning($warning, $stack_ptr, 'MemberVarContainsNumbers', $data);
         }
-
     }//end processMemberVar()
 
 
     /**
      * Processes the variable found within a double quoted string.
      *
-     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-     * @param int                         $stackPtr  The position of the double quoted
+     * @param \PHP_CodeSniffer\Files\File $phpcs_file The file being scanned.
+     * @param int                         $stack_ptr  The position of the double quoted
      *                                               string.
      *
      * @return void
      */
-    protected function processVariableInString(File $phpcsFile, $stackPtr)
+    protected function processVariableInString(File $phpcs_file, $stack_ptr)
     {
-        if ($this->isExcluded($phpcsFile)) {
+        if ($this->isExcluded($phpcs_file)) {
             return;
         }
-        $tokens = $phpcsFile->getTokens();
+        $tokens = $phpcs_file->getTokens();
 
-        if (preg_match_all('|[^\\\]\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)|', $tokens[$stackPtr]['content'], $matches) !== 0) {
-            foreach ($matches[1] as $varName) {
+        if (preg_match_all('|[^\\\]\$([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)|', $tokens[$stack_ptr]['content'], $matches) !== 0) {
+            foreach ($matches[1] as $var_name) {
                 // If it's a php reserved var, then its ok.
-                if (isset($this->phpReservedVars[$varName]) === true) {
+                if (isset($this->phpReservedVars[$var_name]) === true) {
                     continue;
                 }
 
-                if (self::isUnderscoreVariableName($varName) === false) {
+                if (self::isUnderscoreVariableName($var_name) === false) {
                     $error = 'Variable "%s" is not in valid undercase format';
-                    $data  = [$varName];
-                    $phpcsFile->addError($error, $stackPtr, 'StringVarNotUnderscore', $data);
-                } else if (preg_match('|\d|', $varName) === 1) {
+                    $data  = [$var_name];
+                    $phpcs_file->addError($error, $stack_ptr, 'StringVarNotUnderscore', $data);
+                } elseif (preg_match('|\d|', $var_name) === 1) {
                     $warning = 'Variable "%s" contains numbers but this is discouraged';
-                    $data    = [$varName];
-                    $phpcsFile->addWarning($warning, $stackPtr, 'StringVarContainsNumbers', $data);
+                    $data    = [$var_name];
+                    $phpcs_file->addWarning($warning, $stack_ptr, 'StringVarContainsNumbers', $data);
                 }
             }//end foreach
         }//end if
-
     }//end processVariableInString()
 
-    protected static function isUnderscoreVariableName(string $varName) {
-        if ($varName === '_') {
+    protected static function isUnderscoreVariableName(string $var_name)
+    {
+        if ($var_name === '_') {
             return true;
         }
-        if (preg_match("/^[a-z]/", $varName) === 0) {
+        if (preg_match("/^[a-z]/", $var_name) === 0) {
             return false;
         }
         // Check that the name only contains legal characters.
-        $legalChars = 'a-z_0-9';
-        if (preg_match("|[^$legalChars]|", $varName) > 0) {
+        $legal_chars = 'a-z_0-9';
+        if (preg_match("|[^$legal_chars]|", $var_name) > 0) {
             return false;
         }
-        if (strpos($varName, '__') !== false) {
+        if (strpos($var_name, '__') !== false) {
             return false;
         }
         return true;
     }
 
     // Can't get exclude-pattern to work in ruleset.xml, so just hardcode this
-    private function isExcluded(File $phpcsFile) : bool {
-        return preg_match('@[/\\\\]LanguageServer[/\\\\]@', $phpcsFile->path) > 0;
+    private function isExcluded(File $phpcs_file) : bool
+    {
+        return preg_match('@[/\\\\]LanguageServer[/\\\\]@', $phpcs_file->path) > 0;
     }
 }//end class
