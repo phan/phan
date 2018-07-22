83ef0e812810e3116ac41292122e01685673cfd9
Tyson Andre <tysonandre775@hotmail.com>
2018-07-21 21:53:52 -0700

    Change version to 0.12.16-dev

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -26,7 +26,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '0.12.15';
+    const PHAN_VERSION = '0.12.16-dev';
 
     /**
      * List of short flags passed to getopt
