b2e36029a9c2b3af7d8a02b639afe4a8ea107682
Tyson Andre <tysonandre775@hotmail.com>
2018-09-06 21:46:18 -0400

    Change version to Phan 1.0.3-dev

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -27,7 +27,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '1.0.2';
+    const PHAN_VERSION = '1.0.3-dev';
 
     /**
      * List of short flags passed to getopt
