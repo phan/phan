31562c994a02936ad8d78bdaf2fb07846dd120cc
Tyson Andre <tysonandre775@hotmail.com>
2018-10-14 18:48:07 -0400

    reuse comments

---
+++ src/Phan/Language/Element/Comment/NullComment.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+namespace Phan\Language\Element\Comment;
+
+use Phan\Language\Element\Comment;
+use Phan\Language\UnionType;
+use Phan\Library\None;
+
+/**
+ * A comment for an empty doc-block or when comment parsing is disabled
+ */
+final class NullComment extends Comment
+{
+    public function __construct()
+    {
+        $none = new None();
+        $this->throw_union_type = UnionType::empty();
+        $this->closure_scope = $none;
+        $this->inherited_type = $none;
+    }
+
+    /** @var NullComment the only instance of NullComment. Will be immutable. */
+    private static $instance;
+
+    public static function instance() : NullComment {
+        return self::$instance;
+    }
+
+    public static function init() {
+        self::$instance = new NullComment();
+    }
+}
+NullComment::init();
