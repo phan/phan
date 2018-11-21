0297eac1369bd63f96d558ec4a18b9594c927815
Tyson Andre <tysonandre775@hotmail.com>
2018-11-20 19:49:40 -0500

    Change version to 1.1.4-dev

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -30,7 +30,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '1.1.3';
+    const PHAN_VERSION = '1.1.4-dev';
 
     /**
      * List of short flags passed to getopt
