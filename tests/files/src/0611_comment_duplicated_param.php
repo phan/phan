208ce4015143a6ab5c05c7757af26d0f88d27e2b
Daniel Archer <contato@danielcramer.com.br>
2019-01-13 12:00:58 -0200

    Add tests warning repeated elements (#2318)
    * add tests for comment duplicated param
    * add tests for comment duplicated property

---
+++ tests/files/src/0611_comment_duplicated_param.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class C611 {
+    /**
+     * Test for duplicated param var
+     * @param  string $var
+     * @param  string $var
+     * @return void
+     */
+    public function duplicatedParam(string $var)
+    {
+
+    }
+}
