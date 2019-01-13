e320635cd1bed6445883a606787985042116b428
Daniel Archer <contato@danielcramer.com.br>
2019-01-13 03:00:52 -0200

    add tests for comment duplicated property

---
+++ tests/files/src/0612_comment_duplicated_property.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @property mixed $mixedProperty
+ * @property-read int $intReadProperty
+ * @property-read int $intReadProperty
+ * @property-write string $stringWriteProperty
+ * @property mixed $mixedProperty
+ * @property-write string $stringWriteProperty
+ * @property-write string $stringWriteProperty
+ */
+class C612
+{
+}
