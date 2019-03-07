ef809821124f40a3b9310de2e5919ad407e61c0d
Tyson Andre <tysonandre775@hotmail.com>
2019-03-07 14:03:54 -0500

    Add internal data to suggestions for use by plugins

--- src/Phan/Suggestion.php
+++ src/Phan/Suggestion.php
@@ -11,6 +11,9 @@ final class Suggestion
     /** @var string the text of the suggestion */
     private $message;
 
+    /** @var mixed internal data not shown in error messages */
+    private $internal_data;
+
     private function __construct(string $message)
     {
         $this->message = $message;
@@ -38,5 +41,28 @@ final class Suggestion
     public function __toString() : string
     {
         return $this->message;
+    }
+
+    /**
+     * Sets additional data.
+     * This can be used by plugins implementing --automatic-fix, for example.
+     * (Create a Suggestion with the empty string if the suggestion should not be visible in error messages)
+     *
+     * @param mixed $data
+     * @return void
+     * @suppress PhanUnreferencedPublicMethod
+     */
+    public function setInternalData($data) {
+        $this->internal_data = $data;
+    }
+
+    /**
+     * Gets additional data.
+     * This can be used by plugins implementing --automatic-fix, for example.
+     * @return mixed
+     * @suppress PhanUnreferencedPublicMethod
+     */
+    public function getInternalData() {
+        return $this->internal_data;
     }
 }
