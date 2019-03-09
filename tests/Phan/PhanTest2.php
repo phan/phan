88409139ec1f58e2f4093528cc24e35d4c673512
Tyson Andre <tysonandre775@hotmail.com>
2019-03-09 18:18:20 -0500

    Split up the test suite more
    Make `tests/run_all_tests` complete faster on multi-core machines.

---
+++ tests/Phan/PhanTest2.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Phan\Tests;
+
+/**
+ * Runs tests/files/src/0200-0400
+ */
+class PhanTest2 extends PhanTestRange
+{
+    const START_RANGE = '0200';
+    const END_RANGE   = '0400';
+}
