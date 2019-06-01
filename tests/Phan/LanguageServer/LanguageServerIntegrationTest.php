db87b4d7e84059314a5a4c97f255f4ceb754d94c
Tyson Andre <tysonandre775@hotmail.com>
2019-06-01 19:46:17 -0400

    Update integration tests

--- tests/Phan/LanguageServer/LanguageServerIntegrationTest.php
+++ tests/Phan/LanguageServer/LanguageServerIntegrationTest.php
@@ -235,7 +235,7 @@ EOT;
             $this->assertCount(2, $diagnostics);
             // TODO: Pass IssueInstance to the helper instead?
             $this->assertSameDiagnostic($diagnostics[0], Issue::TypeMissingReturn, 1, 'Method \example is declared to return int but has no return value');
-            $this->assertSameDiagnostic($diagnostics[1], Issue::TypeMismatchArgumentInternal, 2, 'Argument 1 (string) is int but \strlen() takes string');
+            $this->assertSameDiagnostic($diagnostics[1], Issue::TypeMismatchArgumentInternal, 2, 'Argument 1 ($string) is int but \strlen() takes string');
 
             $good_file_contents = <<<'EOT'
 <?php
