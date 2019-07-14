f436aa1253dcf8fb09f0dd9d4ce1a051cc69dd3f
Tyson Andre <tysonandre775@hotmail.com>
2019-07-14 15:00:45 -0400

    Back to 2.2.6-dev

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -61,7 +61,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '2.2.5';
+    const PHAN_VERSION = '2.2.6-dev';
 
     /**
      * List of short flags passed to getopt
