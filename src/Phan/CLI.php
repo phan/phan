6ffb3745fd0ab8f0e11117b83079c770f797def1
Tyson Andre <tysonandre775@hotmail.com>
2019-08-12 19:51:45 -0400

    Back to 2.1.11-dev, update README
    There hopefully won't be significant changes to 7.4
    now that we're on php 7.4 betas.

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -62,7 +62,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '2.2.10';
+    const PHAN_VERSION = '2.2.11-dev';
 
     /**
      * List of short flags passed to getopt
