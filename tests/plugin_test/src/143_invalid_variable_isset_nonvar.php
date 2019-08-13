d0a1d55f52e794f3a083bad9e9a4b73b6da82b3d
Tyson Andre <tysonandre775@hotmail.com>
2019-08-13 09:05:50 -0400

    Fix false positive in InvalidVariableIssetPlugin
    Fixes #3089

---
+++ tests/plugin_test/src/143_invalid_variable_isset_nonvar.php
@@ -0,0 +1,19 @@
+<?php
+class X143{
+    /** @var array<string,string> */
+    static $queryCache = [];
+
+    function query(string $hash, string $query): string{
+        if (isset(self::$queryCache[$hash])) { // should not emit PhanPluginUndeclaredVariableIsset
+            return self::$queryCache[$hash];
+        }
+        if (isset($self::$queryCache[$hash])) { // should emit PhanPluginUndeclaredVariableIsset
+            return $self::$queryCache[$hash];
+        }
+        if (isset('bar'->queryCache[$hash])) { // should not crash.
+            return 'x';
+        }
+        return $query;
+    }
+}
+var_export((new X143())->query('foo', 'bar'));
