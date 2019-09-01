92993373eeec75c7aca4feec7e9f09d1bfba8a3d
Daimona <38216014+Daimona@users.noreply.github.com>
2019-09-01 20:46:07 +0200

    Handle references in ClosureReturnTypeOverridePlugin (#3176)
    and add tests

---
+++ tests/files/src/0762_closure_reference.php
@@ -0,0 +1,37 @@
+<?php
+
+function f762() {
+    $x = false;
+    call_user_func_array('f762clos1', [&$x]);
+    if ($x===0.5) {
+        return 'foo';
+    } elseif ($x instanceof \stdClass) {
+        return false;
+    }
+
+    $y = false;
+    call_user_func_array('f762clos2', [ $y ]);
+    if ($y===2) {
+        return 'foo';
+    }
+
+    $z = false;
+    call_user_func_array('f762clos3', [ &$z ]);
+    if ($z === 2) {
+        return 'foo';
+    }
+}
+
+function f762clos1(&$arg) {
+    $arg = rand();
+}
+
+function f762clos2($arg) { // No reference
+    $arg = 2; // @phan-suppress-current-line PhanUnusedVariable
+}
+
+function f762clos3(&$arg) {
+    if (rand()) {
+        $arg = 2;
+    }
+}
