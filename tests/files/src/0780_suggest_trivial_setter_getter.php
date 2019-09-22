eb72b2db3694f92f9b078a995ce5282abb46926f
Tyson Andre <tysonandre775@hotmail.com>
2019-09-22 17:09:40 -0400

    Add tests of edge cases of getters/setters

--- tests/files/src/0780_suggest_trivial_setter_getter.php
+++ tests/files/src/0780_suggest_trivial_setter_getter.php
@@ -10,6 +10,12 @@ class HasGetter780 {
     public function setPropName(string $value) : void {
         $this->propName = $value;;
     }
+    public function badMethod(string $unused_value) : void {
+        'x';
+    }
+    public function badMethod2(string $unused_value = null) : string {
+        0;
+    }
 }
 function test() {
     $g = new HasGetter780();
