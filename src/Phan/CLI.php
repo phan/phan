f1ab7a81c5321196c2e6e91b64951ee34b0dfafc
Tyson Andre <tysonandre775@hotmail.com>
2019-10-03 09:48:03 -0400

    Bump version to 2.2.14-dev

--- src/Phan/CLI.php
+++ src/Phan/CLI.php
@@ -62,7 +62,7 @@ class CLI
     /**
      * This should be updated to x.y.z-dev after every release, and x.y.z before a release.
      */
-    const PHAN_VERSION = '2.2.13';
+    const PHAN_VERSION = '2.2.14-dev';
 
     /**
      * List of short flags passed to getopt
