dd5686451276b7c0d6f88810c0ac5c5f1d031789
Tyson Andre <tysonandre775@hotmail.com>
2019-11-17 16:53:44 -0500

    Make severity of too few arguments to printf critical

--- tests/plugin_test/src/087_sprintf_on_literals.php
+++ tests/plugin_test/src/087_sprintf_on_literals.php
@@ -4,6 +4,6 @@ function expects_int(int $a) {
     var_export($a);
 }
 expects_int(sprintf("Foo %s %d", "arg", "2nd bad arg"));
-expects_int(sprintf("Foo %s"));
+expects_int(sprintf("Foo %s"));  // @phan-suppress-current-line PhanTypeErrorInInternalCall, UnusedPluginSuppression seen in php 8
 expects_int(sprintf("Foo %s", "arg", "extra arg"));
 expects_int(sprintf("Foo %d", 2+3));
