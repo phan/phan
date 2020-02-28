1b8a250b3d3d3c0359128cbb042af5a19f5a0853
Tyson Andre <tysonandre775@hotmail.com>
2020-02-28 17:39:26 -0500

    Fix false positive for MoreSpecificElementTypePlugin

--- .phan/plugins/MoreSpecificElementTypePlugin.php
+++ .phan/plugins/MoreSpecificElementTypePlugin.php
@@ -107,6 +107,11 @@ class MoreSpecificElementTypePlugin extends PluginV3 implements
         if ($declared_return_type->isStrictSubtypeOf($code_base, $actual_type)) {
             return false;
         }
+        if (!$actual_type->asExpandedTypes($code_base)->canCastToUnionType($declared_return_type)) {
+            // Don't warn here about type mismatches such as int->string or object->array, but do warn about SubClass->BaseClass.
+            // Phan should warn elsewhere about those mismatches
+            return false;
+        }
         if ($declared_return_type->hasTopLevelArrayShapeTypeInstances()) {
             return false;
         }
