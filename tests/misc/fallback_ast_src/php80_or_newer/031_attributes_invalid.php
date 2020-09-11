f6a86ddac2b76c430a79e4163ff59a0e94b07813
Tyson Andre <tysonandre775@hotmail.com>
2020-09-10 20:06:41 -0400

    Phan v4: support php 8.0 attributes in polyfill

---
+++ tests/misc/fallback_ast_src/php80_or_newer/031_attributes_invalid.php
@@ -0,0 +1,43 @@
+<?php  // TODO: Start warning about classes that don't define #[Attribute]
+#[MissingAttribute]
+function test31(
+    #[MissingAttribute2, MissingAttribute3()]
+    int $argument
+) {
+    var_export($argument);
+}
+// TODO: Wait for these examples to be supported in tolerant-php-parser
+/*#[Attribute]
+trait TraitAttribute {}
+#[Attribute]
+interface InterfaceAttribute {}
+new #[MyAttr] class { }
+ */
+
+// Anything that's used as an attribute must *directly* have an attribute with the built-in Attribute class.
+#[Attribute]
+class GoodAttribute31 {
+}
+
+class SubGoodAttribute31 extends GoodAttribute31 {}
+
+#[GoodAttribute31]
+function goodExample31() {
+}
+
+#[SubGoodAttribute31, AbstractAttribute]
+function badExample31() {
+}
+test31(31);
+$c = #[TraitAttribute]
+    function() {};
+$c();
+
+$c2 = #[InterfaceAttribute]
+    fn() => true;
+$c2();
+var_export((new ReflectionFunction('goodExample31'))->getAttributes()[0]->newInstance());
+var_export((new ReflectionFunction($c))->getAttributes()[0]->newInstance());
+
+#[Attribute]
+abstract class AbstractAttribute {}
