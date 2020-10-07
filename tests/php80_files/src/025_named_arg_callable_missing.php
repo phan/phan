aebd9b1b3cb974bf832094759af47ca3ac60c277
Tyson Andre <tysonandre775@hotmail.com>
2020-10-07 16:41:42 -0400

    Fix php 8.0 test expectations
    Parameter names changed in php itself

--- tests/php80_files/src/025_named_arg_callable_missing.php
+++ tests/php80_files/src/025_named_arg_callable_missing.php
@@ -11,5 +11,5 @@ call_user_func('C24::main', 1, optionalFlag: true, other: 123);
 call_user_func('C24::main', requiredInt: 0, optionalFlag: true, other: 123);
 call_user_func('C24::main', requiredString: 0, optionalFlag: true, other: 123);
 echo call_user_func('strlen', definitelyInvalidFlag: 'value');
-echo call_user_func('intdiv', dividend: 1000, divisor: 123);
-echo call_user_func('intdiv', divisor: 123);
+echo call_user_func('json_encode', value: 1000, flags: 123);
+echo call_user_func('json_encode', flags: 123);
