758cb53800fafa3eed2fbe5b506701fd1a95ddf1
arm128 <74424954+arm128@users.noreply.github.com>
2021-08-18 15:18:36 -0400

    backslack

--- src/Phan/Plugin/Internal/DependencyGraphPlugin.php
+++ src/Phan/Plugin/Internal/DependencyGraphPlugin.php
@@ -728,7 +728,7 @@ class DependencyGraphVisitor extends PluginAwarePostAnalysisVisitor
             return;
         }
         $called_class_name = (string)$called_class->children['name'];
-        if (\in_array(strtolower($called_class_name), ['self', 'parent', 'class', 'static'], true)) {
+        if (\in_array(\strtolower($called_class_name), ['self', 'parent', 'class', 'static'], true)) {
             return;
         }
         $fqsen = (string)FullyQualifiedClassName::fromStringInContext($called_class_name, $context);
