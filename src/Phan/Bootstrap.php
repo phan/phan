65d7b8d50727ba6487b0d64bb3e357d14d7c4ac6
Tyson Andre <tysonandre775@hotmail.com>
2022-01-10 09:36:11 -0500

    Update AST download link for PHP 8.0 for Windows
    Closes #4645

--- src/Phan/Bootstrap.php
+++ src/Phan/Bootstrap.php
@@ -44,7 +44,7 @@ if (PHP_VERSION_ID < 70200) {
     exit(1);
 }
 
-const LATEST_KNOWN_PHP_AST_VERSION = '1.0.15';
+const LATEST_KNOWN_PHP_AST_VERSION = '1.0.16';
 
 /**
  * Dump instructions on how to install php-ast
@@ -62,8 +62,8 @@ function phan_output_ast_installation_instructions(): void
     }
     if (DIRECTORY_SEPARATOR === '\\') {
         if (PHP_VERSION_ID >= 70300 && PHP_VERSION_ID < 80100 || !preg_match('/[a-zA-Z]/', PHP_VERSION)) {
-            // e.g. https://windows.php.net/downloads/pecl/releases/ast/1.0.15/php_ast-1.0.15-8.0-nts-vs16-x64.zip for php 8.0, 64-bit non thread safe
-            // e.g. https://windows.php.net/downloads/pecl/releases/ast/1.0.15/php_ast-1.0.15-7.4-ts-vc15-x86.zip for php 7.4, 32-bit thread safe
+            // e.g. https://windows.php.net/downloads/pecl/releases/ast/1.0.16/php_ast-1.0.16-8.0-nts-vs16-x64.zip for php 8.0, 64-bit non thread safe
+            // e.g. https://windows.php.net/downloads/pecl/releases/ast/1.0.16/php_ast-1.0.16-7.4-ts-vc15-x86.zip for php 7.4, 32-bit thread safe
             fprintf(
                 STDERR,
                 PHP_EOL . "Windows users can download php-ast from https://windows.php.net/downloads/pecl/releases/ast/%s/php_ast-%s-%s-%s-%s-%s.zip" . PHP_EOL,
@@ -71,7 +71,7 @@ function phan_output_ast_installation_instructions(): void
                 LATEST_KNOWN_PHP_AST_VERSION,
                 PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION,
                 PHP_ZTS ? 'ts' : 'nts',
-                PHP_VERSION_ID >= 80100 ? 'vc16' : 'vc15',
+                PHP_VERSION_ID >= 80000 ? 'vs16' : 'vc15',
                 PHP_INT_SIZE == 4 ? 'x86' : 'x64'
             );
             fwrite(STDERR, "(if that link doesn't work, check https://windows.php.net/downloads/pecl/releases/ast/ )" . PHP_EOL);
