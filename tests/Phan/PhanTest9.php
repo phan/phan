0822eb0c19c01361455354a2ed3270b9989a80dd
Dominik Peters <kuhlesdominik@gmx.de>
2022-02-05 17:44:43 +0100

    Add Phantest9

---
+++ tests/Phan/PhanTest9.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phan\Tests;
+
+/**
+ * Runs tests/files/src/0900-0999
+ */
+class PhanTest9 extends PhanTestRange
+{
+    public const START_RANGE = '0900';
+    public const END_RANGE   = '1000';
+}
