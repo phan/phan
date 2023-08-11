fd32e85dff77a2b4dd686db7edf6a44cfe4402a7
dleibovic <dleibovic@etsy.com>
2023-08-11 17:44:31 -0400

    WIP add tests

--- tests/phound_test/test.sh
+++ tests/phound_test/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -u
 
-if ! which sqlite3 ; then
+if ! type sqlite3 >/dev/null; then
     echo "sqlite3, which is necessary for this test, is not installed!"
     exit 1
 fi
@@ -16,6 +16,53 @@ if ! ../../phan --force-polyfill-parser --memory-limit 1G --analyze-twice ; then
     exit 1
 fi
 
-sqlite3 ~/phound.db 'select * from callsites order by callsite, id'
+ACTUAL=$(sqlite3 ~/phound.db 'select * from callsites order by callsite, id')
+EXPECTED=$(cat <<-EOF
+\A::foo|const|src/001_phound_callsites.php:10|11
+\A::foo|const|src/001_phound_callsites.php:10|12
+\A::foo|prop|src/001_phound_callsites.php:11|21
+\A::foo|prop|src/001_phound_callsites.php:11|22
+\A::fooz|prop|src/001_phound_callsites.php:12|23
+\A::fooz|prop|src/001_phound_callsites.php:12|24
+\A::foo|method|src/001_phound_callsites.php:13|13
+\A::foo|method|src/001_phound_callsites.php:13|14
+\A::bar|method|src/001_phound_callsites.php:14|1
+\A::bar|method|src/001_phound_callsites.php:14|2
+\A::foo|method|src/001_phound_callsites.php:21|15
+\A::foo|method|src/001_phound_callsites.php:21|16
+\A::bar|method|src/001_phound_callsites.php:22|3
+\A::bar|method|src/001_phound_callsites.php:22|4
+\A::foo|method|src/001_phound_callsites.php:24|17
+\A::foo|method|src/001_phound_callsites.php:24|18
+\A::bar|method|src/001_phound_callsites.php:25|5
+\A::bar|method|src/001_phound_callsites.php:25|6
+\A::baz|method|src/001_phound_callsites.php:26|9
+\A::baz|method|src/001_phound_callsites.php:26|10
+\A::bar|method|src/001_phound_callsites.php:31|7
+\A::bar|method|src/001_phound_callsites.php:31|8
+\A::foo|method|src/001_phound_callsites.php:34|19
+\A::foo|method|src/001_phound_callsites.php:34|20
+\Closure::fromCallable|method|src/001_phound_callsites.php:34|25
+\Closure::fromCallable|method|src/001_phound_callsites.php:34|26
+EOF
+)
 
-exit 1
+# diff returns a non-zero exit code if files differ or are missing
+# This outputs the difference between actual and expected output.
+echo
+echo "Comparing the output:"
+
+if type colordiff >/dev/null; then
+    DIFF="colordiff"
+else
+    DIFF="diff"
+fi
+
+$DIFF <(echo "$EXPECTED") <(echo "$ACTUAL")
+EXIT_CODE=$?
+if [ "$EXIT_CODE" == 0 ]; then
+    echo "The sqlite3 DB content matches what was expected"
+else
+    echo "The sqlite3 DB content does not match what was expected"
+    exit $EXIT_CODE
+fi
