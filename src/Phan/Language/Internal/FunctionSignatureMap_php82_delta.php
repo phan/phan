ec37ca09907bc86610ad4f84e9770da7ad52a463
Rasmus Lerdorf <rasmus@lerdorf.com>
2023-10-16 09:19:08 -0700

    Function delta signature map for PHP 8.2

---
+++ src/Phan/Language/Internal/FunctionSignatureMap_php82_delta.php
@@ -0,0 +1,49 @@
+<?php // phpcs:ignoreFile
+return [
+  'added' => [
+      'mysqli_execute_query' => ['mysqli_result|bool', 'query'=>'string', 'params='=>'?array'],
+      'mysqli::execute_query' => ['mysqli_result|bool', 'query'=>'string', 'params='=>'?array'],
+      'curl_upkeep' => ['bool', 'handle'=>'curlhandle'],
+      'imap_is_open' => ['bool', 'imap'=>'imap\connection'],
+      'openssl_cipher_key_length' => ['int|false', 'cipher_algo'=>'string'],
+      'reflectionfunction::isanonymous' => ['bool'],
+      'reflectionmethod::hasprototype' => ['bool'],
+      'sodium_crypto_stream_xchacha20_xor_ic' => ['string', 'message'=>'string', 'nonce'=>'string', 'counter'=>'int', 'key'=>'string'],
+      'memory_reset_peak_usage' => ['void'],
+      'ini_parse_quantity' => ['int', 'shorthand'=>'string'],
+      'libxml_get_external_entity_loader' => ['?callable'],
+  ],
+
+  'changed' => [
+    'dba_open' => [
+      'old' => ['resource|false', 'path'=>'string', 'mode'=>'string', 'handler='=>'string', '...handler_params='=>'string'],
+      'new' => ['resource|false', 'path'=>'string', 'mode'=>'string', 'handler='=>'?string', 'permission='=>'int', 'map_size='=>'int', 'flags='=>'?int']
+    ],
+    'dba_fetch' => [
+      'old' => ['string|false', 'key'=>'string', 'skip'=>'int', 'dba'=>'resource'],
+      'new' => ['string|false', 'key'=>'string|array', 'dba'=>'resource', 'skip='=>'int']
+    ],
+    'iterator_to_array' => [
+      'old' => ['array', 'iterator'=>'Traversable', 'preserve_keys='=>'bool'],
+      'new' => ['array', 'iterator'=>'Traversable|array', 'preserve_keys='=>'bool']
+    ],
+    'iterator_apply' => [
+      'old' => ['int', 'iterator'=>'Traversable', 'callback'=>'callable(mixed):bool', 'args='=>'array'],
+      'new' => ['int', 'iterator'=>'Traversable|iterable', 'callback'=>'callable(mixed):bool', 'args='=>'array'],
+    ],
+    'iterator_count' => [
+      'old' => ['int', 'iterator'=>'Traversable'],
+      'new' => ['int', 'iterator'=>'Traversable|iterable'],
+    ],
+    'iterator_to_array' => [
+      'old' => ['array', 'iterator'=>'Traversable', 'preserve_keys='=>'bool'],
+      'new' => ['array', 'iterator'=>'Traversable|iterable', 'preserve_keys='=>'bool'],
+    ],
+    'IteratorIterator::__construct' => [
+      'old' => ['void', 'iterator'=>'Traversable'],
+      'new' => ['void', 'iterator'=>'Traversable|iterable'],
+    ],
+  ],
+  'removed' => [
+  ],
+];
