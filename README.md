4c590495c66054c8652f11166f6307e5bf83c83a
Tyson Andre <tysonandre775@hotmail.com>
2023-12-26 11:49:45 -0500

    Update documentation of allowed target_php_versions
    Conditional logic for php 8.3 or function signature stubs are not yet created

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ composer require phan/phan
 With Phan installed, you'll want to [create a `.phan/config.php` file](https://github.com/phan/phan/wiki/Getting-Started#creating-a-config-file) in
 your project to tell Phan how to analyze your source code. Once configured, you can run it via `./vendor/bin/phan`.
 
-Phan 5 depends on PHP 7.2+ with the [php-ast](https://github.com/nikic/php-ast) extension (1.0.16+ is preferred) and supports analyzing PHP version 7.0-8.1 syntax.
+Phan 5 depends on PHP 7.2+ with the [php-ast](https://github.com/nikic/php-ast) extension (1.1.1+ is preferred) and supports analyzing PHP version 7.0-8.2 syntax.
 Installation instructions for php-ast can be found [here](https://github.com/nikic/php-ast#installation).
 (Phan can be used without php-ast by using the CLI option `--allow-polyfill-parser`, but there are slight differences in the parsing of doc comments)
 
@@ -126,7 +126,7 @@ A simple `.phan/config.php` file might look something like the following.
 return [
 
     // Supported values: `'5.6'`, `'7.0'`, `'7.1'`, `'7.2'`, `'7.3'`, `'7.4'`,
-    // `'8.0'`, `'8.1'`, `null`.
+    // `'8.0'`, `'8.1'`, `'8.2'`, `'8.3'`, `null`.
     // If this is set to `null`,
     // then Phan assumes the PHP version which is closest to the minor version
     // of the php executable used to execute Phan.
--- src/Phan/Config.php
+++ src/Phan/Config.php
@@ -135,7 +135,7 @@ class Config
         // and checks for undefined classes/methods/functions)
         //
         // Supported values: `'5.6'`, `'7.0'`, `'7.1'`, `'7.2'`, `'7.3'`, `'7.4'`,
-        // `'8.0'`, `'8.1'`, `null`.
+        // `'8.0'`, `'8.1'`, `'8.2'`, `'8.3'`, `null`.
         // If this is set to `null`,
         // then Phan assumes the PHP version which is closest to the minor version
         // of the php executable used to execute Phan.
@@ -147,7 +147,7 @@ class Config
         // The PHP version that will be used for feature/syntax compatibility warnings.
         //
         // Supported values: `'5.6'`, `'7.0'`, `'7.1'`, `'7.2'`, `'7.3'`, `'7.4'`,
-        // `'8.0'`, `'8.1'`, `null`.
+        // `'8.0'`, `'8.1'`, `'8.2'`, `'8.3'`, `null`.
         // If this is set to `null`, Phan will first attempt to infer the value from
         // the project's composer.json's `{"require": {"php": "version range"}}` if possible.
         // If that could not be determined, then Phan assumes `target_php_version`.
@@ -1398,6 +1398,7 @@ class Config
         '8.0' => 70400,
         '8.1' => 80000,
         '8.2' => 80100,
+        '8.3' => 80200,
     ];
 
     private static function computeClosestTargetPHPVersionId(string $version): int
@@ -1408,7 +1409,7 @@ class Config
                 return $resulting_version_id;
             }
         }
-        return 80200;
+        return 80300;
     }
 
     /**
