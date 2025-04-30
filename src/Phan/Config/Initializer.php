ddbacd68f0bf12f9a5acde484821923606e18809
Robert Humphries <rh@tfli.co.uk>
2025-04-30 17:40:29 +0100

    Update determineTargetPHPVersion to support when the Composer version specifies a minimum version of 8.2 (or higher)

--- src/Phan/Config/Initializer.php
+++ src/Phan/Config/Initializer.php
@@ -445,8 +445,10 @@ EOT;
             $version_guess = '7.4';
         } elseif ($version_constraint->matches(self::parseConstraintsForRange('<8.1-dev'))) {
             $version_guess = '8.0';
-        } elseif ($version_constraint->matches(self::parseConstraintsForRange('>=8.1-dev'))) {
+        } elseif ($version_constraint->matches(self::parseConstraintsForRange('<8.2-dev'))) {
             $version_guess = '8.1';
+        } elseif ($version_constraint->matches(self::parseConstraintsForRange('>=8.2-dev'))) {
+            $version_guess = '8.2';
         } else {
             return [null, ['TODO: Choose a target_php_version for this project, or leave as null and remove this comment']];
         }
