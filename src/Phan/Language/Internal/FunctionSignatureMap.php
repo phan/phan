0a0550fccda00754e29e14ad0bb7381499f6f1a0
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-05-27 09:38:35 -0400

    arg to exit is optional

--- src/Phan/Language/Internal/FunctionSignatureMap.php
+++ src/Phan/Language/Internal/FunctionSignatureMap.php
@@ -3077,7 +3077,7 @@ return [
 'exif_read_data' => ['array<string,mixed>|false', 'file'=>'resource|string', 'required_sections='=>'string', 'as_arrays='=>'bool', 'read_thumbnail='=>'bool'],
 'exif_tagname' => ['string|false', 'index'=>'int'],
 'exif_thumbnail' => ['string|false', 'file'=>'string', '&w_width='=>'int', '&w_height='=>'int', '&w_image_type='=>'int'],
-'exit' => ['', 'status'=>'string|int'],
+'exit' => ['', 'status='=>'string|int'],
 'exp' => ['float', 'num'=>'float'],
 'expect_expectl' => ['int', 'expect'=>'resource', 'cases'=>'array', 'match='=>'array'],
 'expect_popen' => ['resource', 'command'=>'string'],
