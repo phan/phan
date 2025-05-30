b039e974b5a76c30bd62136767684bc66f0b9d76
sdolgushev <sdolgushev@etsy.com>
2025-05-30 16:10:12 +0000

    Added tests for functions introduced in PHP 8.4 #2

--- src/Phan/Language/Internal/FunctionSignatureMap_php84_delta.php
+++ src/Phan/Language/Internal/FunctionSignatureMap_php84_delta.php
@@ -6,8 +6,10 @@
  */
 return [
   'added' => [
-      '\Pdo\Pgsql::__construct' => ['void', 'dsn'=>'string', 'username='=>'?string', 'password='=>'?string', 'options='=>'?array'],
-      '\Pdo\Pgsql::setNoticeCallback' => ['void', 'callback'=>'callable'],
+      'pcntl_getcpuaffinity' => ['array|false', 'process_id='=>'int'],
+      'pcntl_waitid' => ['bool', 'idtype='=>'int', 'id='=>'int', '&info='=>'array', 'flags='=>'int'],
+      'Pdo\Pgsql::__construct' => ['void', 'dsn'=>'string', 'username='=>'?string', 'password='=>'?string', 'options='=>'?array'],
+      'Pdo\Pgsql::setNoticeCallback' => ['void', 'callback'=>'callable'],
       'pg_set_chunked_rows_size' => ['bool', 'connection'=>'\PgSql\Connection', 'size'=>'int'],
   ],
   'changed' => [
--- tests/Phan/PHP84Test.php
+++ tests/Phan/PHP84Test.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Phan;
 
-use Phan\Config;
 use Phan\Plugin\ConfigPluginSet;
 use Phan\Tests\AbstractPhanFileTest;
 
