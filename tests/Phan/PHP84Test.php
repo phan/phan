f140dc31e402c0827ead5c2b3e8697c44e4898f3
sdolgushev <sdolgushev@etsy.com>
2025-06-02 16:06:44 +0000

    Use stubs instead of installing extensions for CI tests

--- tests/Phan/PHP84Test.php
+++ tests/Phan/PHP84Test.php
@@ -28,10 +28,13 @@ final class PHP84Test extends AbstractPhanFileTest
         ],
         'plugin_config' => ['infer_pure_methods' => true],
         'autoload_internal_extension_signatures' => [
-            'pdo_pgsql'   => '.phan/internal_stubs/pdo_pgsql.phan_php',
-            'soap'        => '.phan/internal_stubs/soap.phan_php',
-            'tidy'        => '.phan/internal_stubs/tidy.phan_php',
-            'xsl'         => '.phan/internal_stubs/xsl.phan_php',
+            'bcmath'    => '.phan/internal_stubs/pdo_pgsql.phan_php',
+            'intl'      => '.phan/internal_stubs/intl.phan_php',
+            'pdo_pgsql' => '.phan/internal_stubs/pdo_pgsql.phan_php',
+            'pgsql'     => '.phan/internal_stubs/pgsql.phan_php',
+            'soap'      => '.phan/internal_stubs/soap.phan_php',
+            'tidy'      => '.phan/internal_stubs/tidy.phan_php',
+            'xsl'       => '.phan/internal_stubs/xsl.phan_php',
         ],
     ];
 
