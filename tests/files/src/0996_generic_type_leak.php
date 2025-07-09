ccfa188f2efbab596f817f55090c55a70e260173
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-07-09 07:57:56 +0200

    Add some more test cases for template parameter type leaking out
    I kept digging and found a few more edge cases that don't work.

--- tests/files/src/0996_generic_type_leak.php
+++ tests/files/src/0996_generic_type_leak.php
@@ -1,20 +1,59 @@
 <?php
+interface I {}
 
 // Implementation intentionally omitted, this test is just about accessing the property
-
 /** @template T */
 class X {
     /** @var T */
     public $u;
 }
-function getX(): X {
+$x0 = new X;
+$y0 = $x0->u;
+'@phan-debug-var $x0, $y0';
+
+
+function getX1(): X {
     return new X;
 }
-
-$x1 = getX();
+$x1 = getX1();
 $y1 = $x1->u; // whatever the type of this is, it must not be "T"
 '@phan-debug-var $x1, $y1';
 
-$x2 = new X;
+
+/**
+ * This is a mistake, but it still shouldn't leak the template type
+ * @template U
+ * @return X<U>
+ */
+function getX2(): X {
+    return new X;
+}
+$x2 = getX2();
 $y2 = $x2->u;
 '@phan-debug-var $x2, $y2';
+
+
+/**
+ * This more often arises as a result of `if ($x instanceof I) {...}`
+ * @return X&I
+ */
+function getX3() {
+    return new X;
+}
+$x3 = getX3();
+$y3 = $x3->u;
+'@phan-debug-var $x3, $y3';
+
+
+/**
+ * Phan doesn't support extracting types out of an intersection type parameter, but it still shouldn't leak the template type
+ * @template U
+ * @param I&X<U> $x
+ * @return X<U>
+ */
+function getX4(): X {
+    return new X;
+}
+$x4 = getX4();
+$y4 = $x4->u;
+'@phan-debug-var $x4, $y4';
