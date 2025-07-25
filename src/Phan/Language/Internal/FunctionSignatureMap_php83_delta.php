46b78d91a1c56b7792cfcfa34d5fe119b3c24a0c
Sergii Dolgushev <sdolgushev@etsy.com>
2025-07-25 11:21:42 +0100

    Revert "Fix failed 8.3/8.4 alpine tests"
    This reverts commit 3863ac86c208358a4eaa6100d23730559a7a25e3.

--- src/Phan/Language/Internal/FunctionSignatureMap_php83_delta.php
+++ src/Phan/Language/Internal/FunctionSignatureMap_php83_delta.php
@@ -7,8 +7,6 @@
 return [
   'added' => [
       'ldap_connect_wallet' => ['\LDAP\Connection', 'uri='=>'?string', 'wallet'=>'string', 'password'=>'string', 'auth_mode='=>'int'],
-      'posix_fpathconf' => ['int|false', 'file'=>'resource|int', 'name'=>'int'],
-      'posix_pathconf' => ['int|false', 'path'=>'string', 'name'=>'int'],
   ],
   'changed' => [
   ],
