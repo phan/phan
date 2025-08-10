502299f2223d55f66c73457a794a60d8919b580b
Daimona Eaytoy <daimona.wiki@gmail.com>
2025-08-11 01:34:24 +0200

    Respect log_error for invalid maybe-callable FQSENs

--- tests/plugin_test/src/211_callable_union.php
+++ tests/plugin_test/src/211_callable_union.php
@@ -34,6 +34,7 @@ test211_string(['Closure', 'fromCallable']);
 test211_string([]); // these two are errors
 test211_string([1, 2, 3]); // these two are errors
 test211_string('puppies');
+test211_string('not--even**a\\valid//FQSEN');
 
 /**
  * @param callable|class-string $x
@@ -48,7 +49,8 @@ function test211_classstring($x) {
 
 test211_classstring('stdClass');
 test211_classstring('strval');
-test211_classstring('puppies'); // only this one is an error
+test211_classstring('puppies'); // this one is an error
+test211_classstring('not--even**a\\valid//FQSEN'); // this one too
 
 /**
  * @param callable|class-string|string $x
@@ -65,3 +67,4 @@ function test211_classstring_other($x) {
 test211_classstring_other('stdClass');
 test211_classstring_other('strval');
 test211_classstring_other('puppies'); // not an error any more
+test211_classstring_other('not--even**a\\valid//FQSEN'); // not an error
