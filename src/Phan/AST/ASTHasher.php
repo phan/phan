01e03fe9517d05e205f77ab0676834ddcb4c79b1
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-10-02 16:06:23 -0400

    Use polyfill pattern for phan_helpers functions
    Replace runtime function_exists() checks with compile-time polyfills:
    - Add composer autoload file: src/Phan/bootstrap/polyfills.php
    - Define polyfills: src/Phan/polyfills/phan_ast_hash.php
    - Define polyfills: src/Phan/polyfills/phan_unique_types.php
    
    Benefits:
    - No runtime overhead from function_exists() on every call
    - Functions always exist (either C extension or PHP polyfill)
    - Simpler code - just call phan_ast_hash() / phan_unique_types() directly
    - PHP polyfills automatically loaded via Composer autoload
    
    The polyfills use the same algorithms as before (MD5 for ast_hash,
    spl_object_id for unique_types) and are only loaded if the C extension
    is not available.

--- src/Phan/AST/ASTHasher.php
+++ src/Phan/AST/ASTHasher.php
@@ -6,39 +6,18 @@ namespace Phan\AST;
 
 use ast\Node;
 
-use function is_float;
-use function is_int;
 use function is_object;
-use function is_string;
 
 /**
  * This converts a PHP AST Node into a hash.
  * This ignores line numbers and spacing.
+ *
+ * Uses phan_ast_hash() which is provided by either:
+ * - The phan_helpers C extension (fast XXH3-128)
+ * - PHP polyfill (slower MD5, loaded via composer autoload)
  */
 class ASTHasher
 {
-    /** @var bool|null */
-    private static $has_phan_ast_hash = null;
-    /**
-     * @param string|int|null $node
-     * @return string a 16-byte binary key for the array key
-     * @internal
-     */
-    public static function hashKey(int|null|string $node): string
-    {
-        if (is_string($node)) {
-            return md5($node, true);
-        } elseif (is_int($node)) {
-            if (\PHP_INT_SIZE >= 8) {
-                return "\0\0\0\0\0\0\0\0" . \pack('J', $node);
-            } else {
-                return "\0\0\0\0\0\0\0\0\0\0\0\0" . \pack('N', $node);
-            }
-        }
-        // This is not a valid array key, give up
-        return md5((string) $node, true);
-    }
-
     /**
      * @param Node|string|int|float|null $node
      * @return string a 16-byte binary key for the Node which is unlikely to overlap for ordinary code
@@ -46,50 +25,10 @@ class ASTHasher
     public static function hash(\ast\Node|float|int|null|string $node): string
     {
         if (!is_object($node)) {
-            // hashKey
-            if (is_string($node)) {
-                return md5($node, true);
-            } elseif (is_int($node)) {
-                if (\PHP_INT_SIZE >= 8) {
-                    return "\0\0\0\0\0\0\0\0" . \pack('J', $node);
-                } else {
-                    return "\0\0\0\0\0\0\0\0\0\0\0\0" . \pack('N', $node);
-                }
-            } elseif (is_float($node)) {
-                return "\0\0\0\0\0\0\0\1" . \pack('e', $node);
-            } else {
-                // $node must be null
-                return "\0\0\0\0\0\0\0\2\0\0\0\0\0\0\0\0";
-            }
-        }
-        // @phan-suppress-next-line PhanUndeclaredProperty
-        return $node->hash ?? ($node->hash = self::computeHash($node));
-    }
-
-    /**
-     * @return string a newly computed 16-byte binary key
-     */
-    private static function computeHash(Node $node): string
-    {
-        // Check for C extension once per request
-        if (self::$has_phan_ast_hash === null) {
-            self::$has_phan_ast_hash = \function_exists('phan_ast_hash');
-        }
-
-        // Use C extension for AST node hashing if available
-        if (self::$has_phan_ast_hash) {
             return \phan_ast_hash($node);
         }
-
-        $str = 'N' . $node->kind . ':' . ($node->flags & 0xfffff);
-        foreach ($node->children as $key => $child) {
-            // added in PhanAnnotationAdder
-            if (\is_string($key) && \strncmp($key, 'phan', 4) === 0) {
-                continue;
-            }
-            $str .= self::hashKey($key);
-            $str .= self::hash($child);
-        }
-        return md5($str, true);
+        // Cache the hash on the node object to avoid recomputing
+        // @phan-suppress-next-line PhanUndeclaredProperty
+        return $node->hash ?? ($node->hash = \phan_ast_hash($node));
     }
 }
---
+++ src/Phan/polyfills/phan_unique_types.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Polyfill for phan_unique_types() when the phan_helpers extension is not available.
+ *
+ * Fast deduplication of Type objects using object identity.
+ *
+ * @param list<object> $type_list Array of Type objects to deduplicate
+ * @return list<object> Deduplicated array of Type objects
+ */
+function phan_unique_types(array $type_list): array
+{
+    $new_type_list = [];
+    if (\count($type_list) >= 8) {
+        // This approach is faster, but only when there are 8 or more types (tested in php 7.3)
+        // See https://github.com/phan/phan/pull/3475#issuecomment-550570579
+        foreach ($type_list as $type) {
+            $new_type_list[\spl_object_id($type)] = $type;
+        }
+        return \array_values($new_type_list);
+    }
+    foreach ($type_list as $type) {
+        if (!\in_array($type, $new_type_list, true)) {
+            $new_type_list[] = $type;
+        }
+    }
+    return $new_type_list;
+}
