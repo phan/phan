1f50d32e23062205f8a927d0345a16d11193e4f2
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-10-02 16:25:45 -0400

    Fix ASTHasherTest: remove calls to removed hashKey() method
    The hashKey() method was removed as part of the polyfill pattern.
    The hash() method now handles all primitive types directly via
    phan_ast_hash(), so the separate hashKey() tests are redundant.

--- tests/Phan/AST/ASTHasherTest.php
+++ tests/Phan/AST/ASTHasherTest.php
@@ -33,34 +33,28 @@ final class ASTHasherTest extends BaseTest
             $expected = "\0\0\0\0\0\0\0\0\x01\x23\x45\x67\x89\xab\xcd\xef";
             $key = 0x0123456789abcdef;
 
-            $this->assertSameBinaryString($expected, ASTHasher::hashKey($key));
             $this->assertSameBinaryString($expected, ASTHasher::hash($key));
 
             $key = -1;
             $expected = "\0\0\0\0\0\0\0\0\xff\xff\xff\xff\xff\xff\xff\xff";
 
-            $this->assertSameBinaryString($expected, ASTHasher::hashKey($key));
             $this->assertSameBinaryString($expected, ASTHasher::hash($key));
         } else {
             $expected = "\0\0\0\0\0\0\0\0\0\0\0\0\x01\x23\x45\x67";
             $key = 0x01234567;
 
-            $this->assertSameBinaryString($expected, ASTHasher::hashKey($key));
             $this->assertSameBinaryString($expected, ASTHasher::hash($key));
 
             $key = -1;
             $expected = "\0\0\0\0\0\0\0\0\0\0\0\0\xff\xff\xff\xff";
 
-            $this->assertSameBinaryString($expected, ASTHasher::hashKey($key));
             $this->assertSameBinaryString($expected, ASTHasher::hash($key));
         }
         $this->assertSameBinaryString("\0\0\0\0\0\0\0\2\0\0\0\0\0\0\0\0", ASTHasher::hash(null));
         $expected1 = hex2bin('3c6e0b8a9c15224a8228b9a98ca1531d');
-        $this->assertSameBinaryString($expected1, ASTHasher::hashKey('key'));
         $this->assertSameBinaryString($expected1, ASTHasher::hash('key'));
 
         $expected2 = hex2bin('d41d8cd98f00b204e9800998ecf8427e');
-        $this->assertSameBinaryString($expected2, ASTHasher::hashKey(''));
         $this->assertSameBinaryString($expected2, ASTHasher::hash(''));
 
         $expected2 = hex2bin('0000000000000001000000000000f83f');
