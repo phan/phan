phan/phan

commit 143bedddb9af94de02cedc192521a183f0d8cd6e

Fix #4968

src/Phan/Analysis/ParameterTypesAnalyzer.php
@@ -857,10 +857,11 @@ class ParameterTypesAnalyzer
         $comment_parameter_map = null;
         foreach ($phpdoc_parameter_list as $i => $parameter) {
             $parameter_type = $parameter->getNonVariadicUnionType();
+            $comment_parameter_map ??= self::extractCommentParameterMap($method);
+            $comment_parameter = $comment_parameter_map[$parameter->getName()] ?? null;
+
             // If there is already a phpdoc parameter type, then don't bother inheriting the parameter type from $overridden_method
             if (!$parameter_type->isEmpty()) {
-                $comment_parameter_map ??= self::extractCommentParameterMap($method);
-                $comment_parameter = $comment_parameter_map[$parameter->getName()] ?? null;
                 if ($comment_parameter) {
                     $comment_parameter_type = $comment_parameter->getUnionType();
                     if (!$comment_parameter_type->isEmpty()) {
@@ -868,7 +869,7 @@ class ParameterTypesAnalyzer
                     }
                 }
             }
-            // There is no phpdoc parameter for this type
+            // There is no phpdoc parameter for this type, or it was inferred from the signature/default value
 
             $parent_parameter = $o_phpdoc_parameter_list[$i] ?? null;
             if ($parent_parameter) {
@@ -878,7 +879,9 @@ class ParameterTypesAnalyzer
                 }
                 // Allow @inheritDoc to be used to indicate that phpdoc parent parameter types
                 // should override inferred contravariant parameter types.
+                // Also inherit when there's no comment parameter (type may be from default value)
                 if ($parameter_type->isEmpty() ||
+                        !$comment_parameter ||
                         ($parent_parameter_type->isExclusivelyNarrowedFormOf($code_base, $parameter_type) &&
                         ($parameter_type->isExclusivelyArray() || \stripos((string) $method->getDocComment(), '@inheritDoc') !== false))) {
                     $parameter->setUnionType($parent_parameter_type->eraseRealTypeSetRecursively()->withRealTypeSet($parameter_type->getRealTypeSet()));

tests/files/expected/5053_interface_mandatory_param.php.expected
@@ -1,4 +1,4 @@
-%s:47 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test::func(string $a, ?mixed $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:14
-%s:50 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test2::func(string $a, ?mixed $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:23
+%s:47 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test::func(string $a, ?string $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:14
+%s:50 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test2::func(string $a, ?string $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:23
 %s:53 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test3::func(string $a, ?string $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:33
 %s:56 PhanParamTooFewInPHPDoc Call with 1 arg(s) to \Test4::func(string $a, ?string $b = null) which has phpdoc indicating it requires 2 arg(s) ($b is mandatory) defined at %s:42

commit 018a4ac175af3da2b262e5d8cf476f0e997b47f4

Fix parameter type inheritance false positives
When choosing to inherit the parent types, also make sure that there is
no type declaration, and that the type is entirely from the default
value.

This fixes a regression introduced in 143bedd and discovered in bb7097b
(because some tests weren't running).

src/Phan/Analysis/ParameterTypesAnalyzer.php
@@ -916,11 +916,16 @@ class ParameterTypesAnalyzer
                 if ($parent_parameter_type->isEmpty()) {
                     continue;
                 }
+                // Check if the parameter type is exclusively from its default value, and not from the doc comment
+                // or a type declaration (default types are not added as real types).
+                $type_is_exclusively_from_default = !$comment_parameter &&
+                    $parameter->hasDefaultValue() &&
+                    !$parameter->getUnionType()->hasRealTypeSet();
                 // Allow @inheritDoc to be used to indicate that phpdoc parent parameter types
                 // should override inferred contravariant parameter types.
-                // Also inherit when there's no comment parameter (type may be from default value)
+                // Also inherit when the type is exclusively from the default value
                 if ($parameter_type->isEmpty() ||
-                        !$comment_parameter ||
+                        $type_is_exclusively_from_default ||
                         ($parent_parameter_type->isExclusivelyNarrowedFormOf($code_base, $parameter_type) &&
                         ($parameter_type->isExclusivelyArray() || \stripos((string) $method->getDocComment(), '@inheritDoc') !== false))) {
                     $parameter->setUnionType($parent_parameter_type->eraseRealTypeSetRecursively()->withRealTypeSet($parameter_type->getRealTypeSet()));

tests/files/expected/1069_covariant_return_type.php.expected
@@ -1,1 +1,0 @@
-%s:29 PhanTypeMismatchArgumentSuperType Argument 1 ($o) is new BaseClass() of type \NS1069\BaseClass but \NS1069\BarFactory::build() takes \NS1069\SubClass defined at %s:24 (expected type to be the same or a subtype, but saw a supertype instead)