78660cd2b36f06aa88f90a5be3f982d3043fdfd1
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-10-26 07:59:34 -0400

    Fix tests

--- tests/.phan_for_test/config.php
+++ tests/.phan_for_test/config.php
@@ -84,8 +84,17 @@ return [
         'PhanCompatibleTypedClassConstant',  // SPL stub has typed constants from PHP 8.3+
     ],
 
-    // Template extension configuration (for tests that don't load SPL stub)
-    'autoload_internal_extension_signatures_template_classes' => [],
+    // Load SPL stub with template support
+    'autoload_internal_extension_signatures' => [
+        'spl' => PHP_VERSION_ID >= 80400
+            ? '.phan/internal_stubs/spl.phan_php'
+            : '.phan/internal_stubs/spl_php81.phan_php',
+    ],
+
+    // Template extension configuration
+    'autoload_internal_extension_signatures_template_classes' => [
+        'spl',
+    ],
     'autoload_internal_extension_signatures_template_functions' => [],
 
     // Phan will give up on suggesting a different name in issue messages
