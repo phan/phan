492a2e94ed65ed04dc9c297edfc7e9f20096f1e8
Rasmus Lerdorf <rasmus@lerdorf.com>
2025-11-05 20:56:47 -0500

    Revert "Integrate strict_array_checking test into PHPUnit test suite (Issue #5306)"
    This reverts commit d555a423eeaf76f873e0ed18753234476e34a85d.

--- tests/strict_array_checking_test/.phan/config.php
+++ tests/strict_array_checking_test/.phan/config.php
@@ -1,50 +1,40 @@
 <?php
 
 return [
-    'target_php_version' => '8.1',
-
-    'minimum_target_php_version' => '8.1',
-
-    'check_docblock_signature_return_type_match' => true,
-
-    'check_docblock_signature_param_type_match' => true,
-
-    'prefer_narrowed_phpdoc_param_type' => true,
-
-    'ignore_undeclared_functions_with_known_signatures' => true,
-
-    'unused_variable_detection' => true,
-
-    'warn_about_redundant_use_namespaced_class' => true,
-
-    'redundant_condition_detection' => true,
-
-    'infer_default_properties_in_construct' => true,
-
-    'enable_class_alias_support' => true,
-
-    'suppress_issue_types' => [],
-
-    'suggestion_check_limit' => PHP_INT_MAX,
-
-    'max_literal_string_type_length' => 2000,
-
-    'enable_include_path_checks' => true,
-
-    'include_paths' => [
-        '.',
-        \Phan\Config::getProjectRootDirectory() . '/tests/files/include',
-    ],
-
-    'warn_about_relative_include_statement' => true,
-
-    'unused_variable_detection_assume_override_exists' => true,
+    'target_php_version' => null,
 
     'directory_list' => [
         'src'
     ],
 
     'exclude_analysis_directory_list' => [],
+    'exclude_file_list' => [],
+
+    'allow_missing_properties' => false,
+
+    'null_casts_as_any_type' => false,
+
+    'null_casts_as_array' => false,
+
+    'array_casts_as_null' => false,
+
+    'strict_object_checking' => false,
 
     'strict_array_checking' => true,
+
+    'strict_method_checking' => false,
+
+    'strict_param_checking' => false,
+
+    'strict_property_checking' => false,
+
+    'strict_return_checking' => false,
+
+    'ignore_undeclared_variables_in_global_scope' => false,
+
+    'ignore_internal_functions' => false,
+
+    'suppress_issue_types' => [],
+
+    'plugins' => [],
 ];
