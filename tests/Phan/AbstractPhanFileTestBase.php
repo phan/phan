dc488b70a5c0d6c68fc8c9229e770f5ca6798c76
Rasmus Lerdorf <rlerdorf@etsy.com>
2025-11-08 21:28:24 -0500

    Fix bug #5322

--- tests/Phan/AbstractPhanFileTestBase.php
+++ tests/Phan/AbstractPhanFileTestBase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Phan\Tests;
 
 use Phan\Config;
+use Phan\Language\FQSEN\FullyQualifiedGlobalStructuralElement;
 use Phan\Language\Type;
 use Phan\Library\StringUtil;
 use Phan\Output\Collector\BufferingCollector;
@@ -58,6 +59,7 @@ abstract class AbstractPhanFileTestBase extends CodeBaseAwareTestBase
         parent::setUp();
 
         Type::clearAllMemoizations();
+        FullyQualifiedGlobalStructuralElement::clearPreferredNameMap();
     }
 
     /**
@@ -68,6 +70,7 @@ abstract class AbstractPhanFileTestBase extends CodeBaseAwareTestBase
         parent::tearDown();
 
         Type::clearAllMemoizations();
+        FullyQualifiedGlobalStructuralElement::clearPreferredNameMap();
 
         // Force garbage collection to reclaim memory from circular references
         // created by template type instantiations (e.g., SplObjectStorage<T,TInfo>)
