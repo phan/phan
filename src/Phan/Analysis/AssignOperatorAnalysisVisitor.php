502622d7d8a3ae2090fc855a875d005af6376a9c
Rasmus Lerdorf <rlerdorf@etsy.com>
2025-11-15 14:38:25 -0500

    preserve literal results

--- src/Phan/Analysis/AssignOperatorAnalysisVisitor.php
+++ src/Phan/Analysis/AssignOperatorAnalysisVisitor.php
@@ -24,6 +24,8 @@ use Phan\Language\Type;
 use Phan\Language\Type\ArrayType;
 use Phan\Language\Type\FloatType;
 use Phan\Language\Type\IntType;
+use Phan\Language\Type\LiteralFloatType;
+use Phan\Language\Type\LiteralIntType;
 use Phan\Language\Type\MixedType;
 use Phan\Language\Type\NullType;
 use Phan\Language\Type\ScalarType;
@@ -633,7 +635,16 @@ class AssignOperatorAnalysisVisitor extends FlagVisitorImplementation
         return $this->updateTargetWithType($node, function (UnionType $left) use ($node): UnionType {
             $right = UnionTypeVisitor::unionTypeFromNode($this->code_base, $this->context, $node->children['expr']);
             if ($left->isNonNullNumberType() && $right->isNonNullNumberType()) {
-                return IntType::instance(false)->asRealUnionType();
+                $result = BinaryOperatorFlagVisitor::computeIntOrFloatOperationResult($node, $left, $right);
+                return $result->asMappedUnionType(static function (Type $type): Type {
+                    if ($type instanceof LiteralIntType) {
+                        return $type;
+                    }
+                    if ($type instanceof LiteralFloatType) {
+                        return LiteralIntType::instanceForValue((int)$type->getValue(), false);
+                    }
+                    return IntType::instance(false);
+                });
             }
             // TODO: Check if both sides can cast to int and warn if they can't.
             return IntType::instance(false)->asRealUnionType();
--- tests/Phan/Language/UnionTypeTest.php
+++ tests/Phan/Language/UnionTypeTest.php
@@ -143,9 +143,9 @@ final class UnionTypeTest extends TestBase
         $this->assertUnionTypeStringEqual('$x=2; $x *= 3; $x', '6');
         $this->assertUnionTypeStringEqual('$x=2; $x **= 3; $x', '8');
         $this->assertUnionTypeStringEqual('$x=4; $x **= 3.5; $x', '128.0');
-        $this->assertUnionTypeStringEqual('$x=5; $x %= 3; $x', 'int');
-        $this->assertUnionTypeStringEqual('$x=21.2; $x %= 3.5; $x', 'int');
-        $this->assertUnionTypeStringEqual('$x=23.2; $x %= 3.5; $x', 'int');
+        $this->assertUnionTypeStringEqual('$x=5; $x %= 3; $x', '2');
+        $this->assertUnionTypeStringEqual('$x=21.2; $x %= 3.5; $x', '0');
+        $this->assertUnionTypeStringEqual('$x=23.2; $x %= 3.5; $x', '2');
         $this->assertUnionTypeStringEqual('$x=5;    $x ^= 3;    $x', '6');
         $this->assertUnionTypeStringEqual('$x="ab"; $x ^= "ac"; $x', 'string');
         $this->assertUnionTypeStringEqual('$x=5;    $x |= 3;    $x', '7');
