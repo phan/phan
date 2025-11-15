d713f87a65e31c5f9ad6b5b7d3c70d9635f81277
Rasmus Lerdorf <rlerdorf@etsy.com>
2025-11-15 12:35:04 -0500

    Fix CI

--- src/Phan/Analysis/ParameterTypesAnalyzer.php
+++ src/Phan/Analysis/ParameterTypesAnalyzer.php
@@ -315,14 +315,14 @@ class ParameterTypesAnalyzer
             );
             return;
         }
-        $checked_class_override = false;
+        $processed_class_override = false;
         foreach ($overridden_method_list as $overridden_method) {
             $overridden_class = $overridden_method->getClass($code_base);
             if (!$overridden_class->isInterface()) {
-                if ($checked_class_override) {
+                if ($processed_class_override) {
                     continue;
                 }
-                $checked_class_override = true;
+                $processed_class_override = true;
             }
             self::analyzeOverrideSignatureForOverriddenMethod($code_base, $method, $class, $overridden_method);
         }
