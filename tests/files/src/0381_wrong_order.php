8282b4caf21ff687d3d2f792e998bd3bc5f3c98c
Rasmus Lerdorf <rlerdorf@etsy.com>
2026-06-22 15:27:27 +0000

    Fixes 5547

--- tests/files/src/0381_wrong_order.php
+++ tests/files/src/0381_wrong_order.php
@@ -6,3 +6,6 @@ $value = 'value';
 preg_replace($value, 'subst', '/v/');  // Phan should warn
 $result = stripos('v', $value);  // Phan should warn
 $result = mb_stripos('v', $value);  // Phan should warn
+
+// Named arguments make $args sparse - should not crash (issue #5533)
+$csv = str_getcsv("abc,def", escape: '');
